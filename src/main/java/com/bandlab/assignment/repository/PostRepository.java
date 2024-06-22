fde7465466ae432788e32307e7d46cc41b7c6b33
Mohan Singh <mohan.singh.upr@gmail.com>
2024-06-22 17:01:52 +0530

    Add proper pagesize support

--- src/main/java/com/bandlab/assignment/repository/PostRepository.java
+++ src/main/java/com/bandlab/assignment/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.bandlab.assignment.dto.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,6 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("SELECT p FROM Post p LEFT JOIN FETCH p.comments c WHERE p.id >= :start AND (c.id IN (SELECT c2.id FROM Comment c2 WHERE c2.post.id = p.id ORDER BY c2.createdAt DESC LIMIT 2) OR NOT EXISTS (SELECT c2.id FROM Comment c2 WHERE c2.post.id = p.id)) ORDER BY p.createdAt DESC")
-    List<Post> getPosts(Long start, Pageable pageable);
+    List<Post> getPosts(@Param("start") Long start, Pageable pageable);
 
 }
